5fbd32c216e32b1fb2a886b0c6dd41a69fee3c45
Umesh Kumar <umeshsingla05@gmail.com>
2016-03-15 00:49:41 +0530

    Updated pot file and version number

--- pro-sites.php
+++ pro-sites.php
@@ -33,7 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 class ProSites {
 
-	var $version = '3.5.212';
+	var $version = '3.5.2';
 	var $location;
 	var $language;
 	var $plugin_dir = '';
