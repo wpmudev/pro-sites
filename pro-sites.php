dd84b8347d254ec3e755e84b6cc85d88f517729f
Lindeni Mahlalela <mahlamusa@gmail.com>
2017-06-02 14:45:47 +0200

    Pro Sites Manually extend trial

--- pro-sites.php
+++ pro-sites.php
@@ -2186,12 +2186,6 @@ Thanks!", 'psts' ),
 		}
 		
 		
-		if( 'manual' === $gateway && $exists ) {
-            //control whether we are upgrading the user or extending trial period
-            $extra_sql .= ", gateway = '" . ( $extend_type == "trial" ?  'trial' : 'manual' ) . "'";            
-        } else {
-		
-		
 		$extra_sql .= ( $amount ) ? $wpdb->prepare( ", amount = %s", $amount ) : '';
 		$extra_sql .= ( $term ) ? $wpdb->prepare( ", term = %d", $term ) : '';
 		$extra_sql .= $wpdb->prepare( ", is_recurring = %d", $is_recurring );
