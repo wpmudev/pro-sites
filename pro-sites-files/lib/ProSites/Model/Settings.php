e0c5d82bd35da501873384efffe87a270ac5b9f2
Lindeni Mahlalela <mahlamusa@gmail.com>
2017-06-02 12:37:28 +0200

    Prosites Page or post quota settings refresh on selected level changed

--- pro-sites-files/lib/ProSites/Model/Settings.php
+++ pro-sites-files/lib/ProSites/Model/Settings.php
@@ -48,7 +48,6 @@ if ( ! class_exists( 'ProSites_Model_Settings' ) ) {
 				$levels_quotas[$quotas_for_level] = $_POST['psts']['pq_quotas'];
 				
 				$per_level = isset($_POST['psts']['per_level'])? $_POST['psts']['per_level'] : 0 ;
-				error_log( "Per level : $per_level ");
 				//add or update levels quotas in old settings before medging old_settings below
 				$old_settings['levels_quotas'] = $levels_quotas;
 				$old_settings['levels_quotas']['per_level'] = $per_level;
